d0f279fc265469ae78c12c3ca5b7de99e41b3bc3
aremanhashemi <aremanhashemi@gmail.com>
2021-12-31 13:52:53 -0800

    Commented out UserPool, added package-lock.json to .gitignore, added README instructions

--- src/LoginPage/UserPool.js
+++ src/LoginPage/UserPool.js
@@ -1,9 +1,9 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
+// import { CognitoUserPool } from "amazon-cognito-identity-js";
 
 
-const poolData = {
-    UserPoolId: "us-west-1_AjH6u46W6",
-    ClientId: "3jevanqtlq8fo8cmf4ph09sjg"
-}
+// const poolData = {
+//     UserPoolId: "us-west-1_AjH6u46W6",
+//     ClientId: "3jevanqtlq8fo8cmf4ph09sjg"
+// }
 
-export default new CognitoUserPool(poolData)
+// export default new CognitoUserPool(poolData)
