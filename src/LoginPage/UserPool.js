b6530634ed466167bb8af4ab000e2b2e51125702
Areman Hashemi <aremanhashemi@gmail.com>
2022-01-08 11:29:45 -0800

    Removing userpool

--- src/LoginPage/UserPool.js
+++
@@ -1,9 +0,0 @@
-// import { CognitoUserPool } from "amazon-cognito-identity-js";
-
-
-// const poolData = {
-//     UserPoolId: "us-west-1_AjH6u46W6",
-//     ClientId: "3jevanqtlq8fo8cmf4ph09sjg"
-// }
-
-// export default new CognitoUserPool(poolData)
